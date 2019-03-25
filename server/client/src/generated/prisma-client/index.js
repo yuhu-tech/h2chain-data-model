199364259141f596ac069620697eadb3b4f07d85
lutianzhou001 <lutianzhou@yuhu.tech>
2019-03-25 23:26:22 +0800

    build the three models, I will change the client model later

---
+++ server/client/src/generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Profile",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:6666`
+});
+exports.prisma = new exports.Prisma();
