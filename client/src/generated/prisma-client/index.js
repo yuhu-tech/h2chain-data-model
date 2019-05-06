edbb3d48b6ec1475d87c90e10f617cd46ec2a6a0
lutianzhou001 <lutianzhou@yuhu.tech>
2019-05-06 20:34:50 +0800

    modified datamodel

---
+++ client/src/generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Personalmsg",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:3366`
+});
+exports.prismaClient = new exports.Prisma();
---
+++ hotel/src/generated/prisma-client/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Profile",
+    embedded: false
+  },
+  {
+    name: "Occupation",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:4466`
+});
+exports.prismaHotel = new exports.Prisma();
